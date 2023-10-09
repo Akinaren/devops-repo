cf4cd7319207023dfedd4eb86853759bb46b2b1f
akhi <akhi@gmail.com>
2023-10-09 14:16:20 +0530

    2nd commit

---
+++ f1.java
@@ -0,0 +1,10 @@
+this is a java file
+import java.until.Scanner;
+public static void main(String[] args) {
+	Scanner reader = new Scanner(System.in);
+	System.out.print("Enter a number:");
+	int number = reader.nextInt();
+	System.out.println("you entered:" + number);
+}
+}
+
